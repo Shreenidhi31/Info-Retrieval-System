c1114bae3869879307cada054f2b9efc841b95e7
Shreenidhi31 <Shreenidhirbhat@gmail.com>
2025-09-18 18:22:05 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = "Gen AI Project",
+    version = '0.0.0',
+    author = "nid",
+    author_email= "flyingoversky30@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+
+)
